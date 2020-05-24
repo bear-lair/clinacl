0c3b9eb1d7a83bc8ebf797c326f06b0717f184ff
LADYBEAR7820 <54520703+LADYBEAR7819@users.noreply.github.com>
2020-05-24 07:54:48 -0500

    Me
    {
      "body": {
        "key": {
          "eldest_kid": "01204a98cabd61756edb40d3830ca1c8a98fc63ba4fbd3551ea53fc40f2db7d1dd1a0a",
          "host": "keybase.io",
          "kid": "01204a98cabd61756edb40d3830ca1c8a98fc63ba4fbd3551ea53fc40f2db7d1dd1a0a",
          "uid": "26763db22f02427ad0421bb64cd9be19",
          "username": "ladybear7820"
        },
        "merkle_root": {
          "ctime": 1589993707,
          "hash": "dbf67b5789de8faaa7b84ad43811fc643fa19315824960657a7ce02ab062b6da5878b1131b38c30f2953cbb9752124daebfce6a729e39db55c9a5d3773937c70",
          "hash_meta": "1cb900cd254a192f3c6b8346200c95e91dd23af6b704bdbcc30a018da2763ae9",
          "seqno": 16358388
        },
        "service": {
          "entropy": "0tFyjtExeaMAaGzbn5yxDXPE",
          "name": "twitter",
          "username": "seannaprice1"
        },
        "type": "web_service_binding",
        "version": 2
      },
      "client": {
        "name": "keybase.io go client",
        "version": "5.4.1"
      },
      "ctime": 1589993740,
      "expire_in": 504576000,
      "prev": "eb9a650a3f63feb74e88f5877a8e05cbb5333cd2f7239d6d3245852e69bcd3cb",
      "seqno": 4,
      "tag": "signature"
    }
          "host": "keybase.io",
          "kid": "01204a98cabd61756edb40d3830ca1c8a98fc63ba4fbd3551ea53fc40f2db7d1dd1a0a",
          "uid": "26763db22f02427ad0421bb64cd9be19",
          "username": "ladybear7820"
        },
        "merkle_root": {
          "ctime": 1589993707,
          "hash": "dbf67b5789de8faaa7b84ad43811fc643fa19315824960657a7ce02ab062b6da5878b1131b38c30f2953cbb9752124daebfce6a729e39db55c9a5d3773937c70",
          "hash_meta": "1cb900cd254a192f3c6b8346200c95e91dd23af6b704bdbcc30a018da2763ae9",
          "seqno": 16358388
        },
        "service": {
          "entropy": "0tFyjtExeaMAaGzbn5yxDXPE",
          "name": "twitter",
          "username": "seannaprice1"
        },
        "type": "web_service_binding",
        "version": 2
      },
      "client": {
        "name": "keybase.io go client",
        "version": "5.4.1"
      },
      "ctime": 1589993740,
      "expire_in": 504576000,
      "prev": "eb9a650a3f63feb74e88f5877a8e05cbb5333cd2f7239d6d3245852e69bcd3cb",
      "seqno": 4,
      "tag": "signature"hKRib2R5hqhkZXRhY2hlZMOpaGFzaF90eXBlCqNrZXnEIwEgSpjKvWF1bttA04MMocipj8Y7pPvTVR6lP8QPLbfR3RoKp3BheWxvYWTESpcCBMQg65plCj9j/rdOiPWHeo4Fy7UzPNL3I51tMkWFLmm808vEIAuojYZO4+yQNQQOaHiFtg/9UAbCVSSEmd2aq6qQYUeNAgHCo3NpZ8RAsXgBeWdc2jVjNkrgl6/4pNyrSPaxwIJZ6o/UXT3RKGX7b0REd8KwT4qvySsVHe3Z30dahs0YrSdpncu2aHjTBKhzaWdfdHlwZSCkaGFzaIKkdHlwZQildmFsdWXEILIzhStdBh9FCD+iZekFD/NkIrD+iVOIAz841pVc3O0So3RhZ80CAqd2ZXJzaW9uAQ==
    }Verifying myself: I am ladybear7820 on Keybase.io. T0LqbBsiQxOpPaxHa-6jabl9CXAFmBO2YPPe hKRib2R5hqhkZXRhY2hlZMOpaGFzaF90eXBlCqNrZXnEIwEgSpjKvWF1bttA04MMocipj8Y7pPvTVR6lP8QPLbfR3RoKp3BheWxvVerifying myself: I am ladybear7820 on Keybase.io. T0LqbBsiQxOpPaxHa-6jabl9CXAFmBO2YPPe /

--- setup.py
+++ setup.py
@@ -17,4 +17,65 @@ setuptools.setup(
         ]
     },
     install_requires=['docopt', 'u-msgpack-python', 'pynacl', 'six']
-)
+){
+  "body": {
+    "key": {
+      "eldest_kid": "01204a98cabd61756edb40d3830ca1c8a98fc63ba4fbd3551ea53fc40f2db7d1dd1a0a",
+      "host": "keybase.io",
+      "kid": "01204a98cabd61756edb40d3830ca1c8a98fc63ba4fbd3551ea53fc40f2db7d1dd1a0a",
+      "uid": "26763db22f02427ad0421bb64cd9be19",
+      "username": "ladybear7820"
+    },
+    "merkle_root": {
+      "ctime": 1589993707,
+      "hash": "dbf67b5789de8faaa7b84ad43811fc643fa19315824960657a7ce02ab062b6da5878b1131b38c30f2953cbb9752124daebfce6a729e39db55c9a5d3773937c70",
+      "hash_meta": "1cb900cd254a192f3c6b8346200c95e91dd23af6b704bdbcc30a018da2763ae9",
+      "seqno": 16358388
+    },
+    "service": {
+      "entropy": "0tFyjtExeaMAaGzbn5yxDXPE",
+      "name": "twitter",
+      "username": "seannaprice1"
+    },
+    "type": "web_service_binding",
+    "version": 2
+  },
+  "client": {
+    "name": "keybase.io go client",
+    "version": "5.4.1"
+  },
+  "ctime": 1589993740,
+  "expire_in": 504576000,
+  "prev": "eb9a650a3f63feb74e88f5877a8e05cbb5333cd2f7239d6d3245852e69bcd3cb",
+  "seqno": 4,
+  "tag": "signature"
+}
+      "host": "keybase.io",
+      "kid": "01204a98cabd61756edb40d3830ca1c8a98fc63ba4fbd3551ea53fc40f2db7d1dd1a0a",
+      "uid": "26763db22f02427ad0421bb64cd9be19",
+      "username": "ladybear7820"
+    },
+    "merkle_root": {
+      "ctime": 1589993707,
+      "hash": "dbf67b5789de8faaa7b84ad43811fc643fa19315824960657a7ce02ab062b6da5878b1131b38c30f2953cbb9752124daebfce6a729e39db55c9a5d3773937c70",
+      "hash_meta": "1cb900cd254a192f3c6b8346200c95e91dd23af6b704bdbcc30a018da2763ae9",
+      "seqno": 16358388
+    },
+    "service": {
+      "entropy": "0tFyjtExeaMAaGzbn5yxDXPE",
+      "name": "twitter",
+      "username": "seannaprice1"
+    },
+    "type": "web_service_binding",
+    "version": 2
+  },
+  "client": {
+    "name": "keybase.io go client",
+    "version": "5.4.1"
+  },
+  "ctime": 1589993740,
+  "expire_in": 504576000,
+  "prev": "eb9a650a3f63feb74e88f5877a8e05cbb5333cd2f7239d6d3245852e69bcd3cb",
+  "seqno": 4,
+  "tag": "signature"hKRib2R5hqhkZXRhY2hlZMOpaGFzaF90eXBlCqNrZXnEIwEgSpjKvWF1bttA04MMocipj8Y7pPvTVR6lP8QPLbfR3RoKp3BheWxvYWTESpcCBMQg65plCj9j/rdOiPWHeo4Fy7UzPNL3I51tMkWFLmm808vEIAuojYZO4+yQNQQOaHiFtg/9UAbCVSSEmd2aq6qQYUeNAgHCo3NpZ8RAsXgBeWdc2jVjNkrgl6/4pNyrSPaxwIJZ6o/UXT3RKGX7b0REd8KwT4qvySsVHe3Z30dahs0YrSdpncu2aHjTBKhzaWdfdHlwZSCkaGFzaIKkdHlwZQildmFsdWXEILIzhStdBh9FCD+iZekFD/NkIrD+iVOIAz841pVc3O0So3RhZ80CAqd2ZXJzaW9uAQ==
+}Verifying myself: I am ladybear7820 on Keybase.io. T0LqbBsiQxOpPaxHa-6jabl9CXAFmBO2YPPe hKRib2R5hqhkZXRhY2hlZMOpaGFzaF90eXBlCqNrZXnEIwEgSpjKvWF1bttA04MMocipj8Y7pPvTVR6lP8QPLbfR3RoKp3BheWxvVerifying myself: I am ladybear7820 on Keybase.io. T0LqbBsiQxOpPaxHa-6jabl9CXAFmBO2YPPe /
